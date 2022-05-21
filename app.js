ed3334f0a5759860e562d90ec03356ab7024cf5e
Quydev <quypham.devplus@gmail.com>
2022-05-21 17:04:09 +0700

    :sparkles: Define frame BE :sparkles:

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+//Router
+
+//Error Handle
+
+// Connect MongoDB
+const URI = process.env.MONGODB_URL;
+mongoose.connect(
+  URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to MongoDB");
+  }
+);
+
+module.exports = app;
